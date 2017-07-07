37ebfed39d6aaf8ce238ed6abc86a5c80594c79b
Danil <danil.kononyhin@mail.ru>
2017-07-07 14:50:45 +0300

    Add normal messages

--- Analyzer1/Analyzer1/DiagnosticAnalyzer.cs
+++ Analyzer1/Analyzer1/DiagnosticAnalyzer.cs
@@ -13,6 +13,12 @@ namespace Analyzer1
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class Analyzer1Analyzer : DiagnosticAnalyzer
     {
+        public const int ConstField = 0;
+        public const int ConstProp = 0;
+        public const int ConstPar = 0;
+        public const int ConstNameSp = 0;
+        public const int ConstMeth = 0;
+        public const int ConstClass = 0;
         public const string DiagnosticId = "Analyzer1";
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
@@ -25,30 +31,88 @@ namespace Analyzer1
 
         public override void Initialize(AnalysisContext context)
         {
-            context.RegisterSyntaxNodeAction(AnalyzeNode,  SyntaxKind.ClassDeclaration );
-           // context.RegisterSymbolAction(AnalyzeSymbol, );
-              
+            context.RegisterSyntaxNodeAction(AnalyzeClassNode,  SyntaxKind.ClassDeclaration );
+            context.RegisterSyntaxNodeAction(AnalyzeVarNode, SyntaxKind.VariableDeclaration);
+            //context.RegisterSemanticModelAction(AnalyzeSymbol);
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Field);
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Namespace);
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Method);
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Property);
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Parameter);
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
         }
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+
+        private void AnalyzeSymbol(SymbolAnalysisContext context)
         {
-            return;
+            var TypeSymbol = context.Symbol;
+            if (TypeSymbol is IFieldSymbol)
+            {
+                if (TypeSymbol.Name.Length > ConstField)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, TypeSymbol.Locations[0]));
+                }
+            }
+            if (TypeSymbol is IPropertySymbol)
+            {
+                if (TypeSymbol.Name.Length > ConstProp)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, TypeSymbol.Locations[0]));
+                }
+            }
+            if (TypeSymbol is IMethodSymbol)
+            {
+                if (TypeSymbol.Name.Length > ConstMeth)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, TypeSymbol.Locations[0]));
+                }
+            }
+            if (TypeSymbol is INamespaceSymbol)
+            {
+                if (TypeSymbol.Name.Length > ConstNameSp)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, TypeSymbol.Locations[0]));
+                }
+            }
+            if (TypeSymbol is IParameterSymbol)
+            {
+                if (TypeSymbol.Name.Length > ConstPar)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, TypeSymbol.Locations[0]));
+                }
+            }
+            if (TypeSymbol is IParameterSymbol)
+            {
+                if (TypeSymbol.Name.Length > ConstClass)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, TypeSymbol.Locations[0]));
+                }
+            }
         }
-        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+
+        private static void AnalyzeClassNode(SyntaxNodeAnalysisContext context)
         {
 
             var IdName = (ClassDeclarationSyntax)context.Node;
-            // Perform data flow analysis on the local declarartion.
-            //var dataFlowAnalysis = context.SemanticModel.AnalyzeDataFlow(IdName);
-
-            // Retrieve the local symbol for each variable in the local declaration
-            // and ensure that it is not written outside of the data flow analysis region.
             if (IdName.Identifier.ToString().Length > 5)
             {
                 context.ReportDiagnostic(Diagnostic.Create(Rule, IdName.Identifier.GetLocation()));
             }
         }
-                
-            
-        
+
+        private static void AnalyzeVarNode(SyntaxNodeAnalysisContext context)
+        {
+
+            var IdName = (VariableDeclarationSyntax)context.Node;
+            foreach (var variable in IdName.Variables)
+            {
+                if (variable.Identifier.ToString().Length > 6)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, variable.Identifier.GetLocation()));
+                }
+            }
+        }
+
+
+
     }
 }
